acd445ffc583340406f6f926adcae4a673c5bda8
CPU_Code <923992029@qq.com>
2020-05-05 20:36:52 +0800

    往一个描述符写n字节_未完

---
+++ UNIX_net/lib/writen.c
@@ -0,0 +1,48 @@
+/*
+ * @Author: cpu_code
+ * @Date: 2020-05-05 20:14:04
+ * @LastEditTime: 2020-05-05 20:36:45
+ * @FilePath: \linux_network\UNIX_net\lib\writen.c
+ * @Gitee: https://gitee.com/cpu_code
+ * @CSDN: https://blog.csdn.net/qq_44226094
+ */
+/* include writen */
+#include	"unp.h"
+
+/* Write "n" bytes to a descriptor. */
+ssize_t writen(int fd, const void *vptr, size_t n)
+{
+    size_t		nleft;
+    ssize_t		nwritten;
+    const char	*ptr;
+
+    ptr = vptr;
+    nleft = n;
+
+    while(nleft > 0)
+    {
+        if((nwritten = write(fd, ptr, nleft)) <= 0)
+        {
+            if(nwritten < 0 && errno == EINTR)
+            {
+                nwritten = 0;		/* and call write() again */
+            }
+            else
+            {
+                return(-1);			/* error */
+            }
+        }
+        nleft -= nwritten;
+        ptr   += nwritten;
+    }
+    return(n);
+}
+
+void Writen(int fd, void *ptr, size_t nbytes)
+{
+	if (writen(fd, ptr, nbytes) != nbytes)
+    {
+		err_sys("writen error");
+    }
+
+}
