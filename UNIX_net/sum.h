d2a870bef43c79cb8c84d5a05873ccdf1cdeee79
CPU_Code <923992029@qq.com>
2020-05-07 16:49:15 +0800

    和的头文件

---
+++ UNIX_net/sum.h
@@ -0,0 +1,18 @@
+/*
+ * @Author: cpu_code
+ * @Date: 2020-05-07 16:41:04
+ * @LastEditTime: 2020-05-07 16:44:17
+ * @FilePath: \linux_network\UNIX_net\sum.h
+ * @Gitee: https://gitee.com/cpu_code
+ * @CSDN: https://blog.csdn.net/qq_44226094
+ */
+struct args 
+{
+  long	arg1;
+  long	arg2;
+};
+
+struct result 
+{
+  long	sum;
+};
